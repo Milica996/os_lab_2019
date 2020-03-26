9f166f1f58fe74fdb0ef5878ff0013fca55236b6
milica996 <milicab98@gmail.com>
2020-03-26 15:40:05 +0000

    static and dinamic

--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -9,3 +9,9 @@ void Swap(char *left, char *right)
 
 // gcc main.c swap.c -o swap
 // ./swap
+
+// gcc -c revert_string.c -o revert_string.o                    
+// ar rc libMY_STATIC.a revert_string.o
+// ranlib libMY_STATIC.a
+// gcc main.c libMY_STATIC.a
+// ./a.out milica
